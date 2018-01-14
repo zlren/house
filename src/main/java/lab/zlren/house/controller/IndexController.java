43ca5c5b57bea0df698439a4dfa07f284543d5ab
zlren <zlren2012@163.com>
2018-01-14 21:25:44 +0800

    spring-boot自动配置

--- src/main/java/lab/zlren/house/controller/IndexController.java
+++ src/main/java/lab/zlren/house/controller/IndexController.java
@@ -1,5 +1,7 @@
 package lab.zlren.house.controller;
 
+import org.apache.http.client.HttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private HttpClient httpClient;
 
     @GetMapping("index")
     public String index() {
